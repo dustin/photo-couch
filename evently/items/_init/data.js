193e38f5a42b8b33ba44c86d6caf40c92948be8f
Dustin Sallings <dustin@spy.net>
2011-01-22 16:44:36 -0800

    Index page should show 10 random pictures.

--- evently/items/_init/data.js
+++ evently/items/_init/data.js
@@ -1,8 +1,12 @@
 function(data) {
-  var p;
-  return {
-    items : data.rows.map(function(r) {
-                              return r.value;
-                          })
+  var all_items = data.rows.map(function(r) { return r.value; });
+  var items = [];
+  /* Randomly grab 10 UOT pictures */
+  while (all_items.length > 0 && items.length < 10) {
+      var offset = Math.floor(Math.random() * all_items.length);
+      var anItem = all_items[offset];
+      items.push(anItem);
+      all_items.splice(offset, 1);
   }
-};
+  return { items: items };
+}
