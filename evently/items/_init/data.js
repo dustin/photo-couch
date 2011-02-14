49cb08ad0aef5fad672951f0b67ab792dac0e240
Dustin Sallings <dustin@spy.net>
2011-02-13 16:27:37 -0800

    Clean the marked-down data before generating js code.
    Markdown makes it semi-safe, but doesn't make it able to be placed in
    a string.  Still need to swab that up.

--- evently/items/_init/data.js
+++ evently/items/_init/data.js
@@ -6,6 +6,10 @@ function(data) {
   var all_items = data.rows.map(function(r) { return r.value; });
   var items = [];
 
+  function cleanString(s) {
+      return s.replace(/"/g, "&quot;").replace(/([\r\n])/g, " ");
+  }
+
   /* Randomly grab 10 UOT pictures */
   while (all_items.length > 0 && items.length < 10) {
       var offset = Math.floor(Math.random() * all_items.length);
@@ -17,7 +21,7 @@ function(data) {
                        return "<a href=" + l + ">" + t + "</a>";
                       });
       anItem.cleanDescr = anItem.taken + " - (" + taglinks.join(", ") + ")" +
-                              markdown.encode(anItem.descr);
+                              cleanString(markdown.encode(anItem.descr));
       anItem.showLink = path.show('item', anItem._id);
       anItem.imageLink = path.attachment(anItem._id, '800x600.' + anItem.extension);
       items.push(anItem);
