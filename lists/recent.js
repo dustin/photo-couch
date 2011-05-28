47be78b57e29efcd0a9ae4413a58240fb8ce4bf1
Dustin Sallings <dustin@spy.net>
2011-05-27 20:39:38 -0700

    Some basic pagination on recent photos page.

--- lists/recent.js
+++ lists/recent.js
@@ -1,30 +1,36 @@
 function(head, req) {
-	// !json templates.head
-	// !json templates.recent
+    // !json templates.head
+    // !json templates.recent
 
-	provides("html", function() {
-		var row;
+    provides("html", function() {
+        var row;
 
-		var data = {
-			title: "Recent Photos",
+        var data = {
+            title: "Recent Photos",
             mainid: "thumblist"
-		};
+        };
 
         var Mustache = require("vendor/couchapp/lib/mustache");
         var path = require("vendor/couchapp/lib/path").init(req);
 
-		send(Mustache.to_html(templates.head, data));
+        send(Mustache.to_html(templates.head, data));
 
-		while(row = getRow()) {
-			send(Mustache.to_html(templates.recent.row, {
-				id: row.value._id,
+        var lastKey = "";
+        while( (row = getRow()) ) {
+            lastKey = row.key;
+            send(Mustache.to_html(templates.recent.row, {
+                id: row.value._id,
                 ts: row.value.ts,
                 taken: row.value.taken,
-				show: path.show('item', row.value._id),
+                show: path.show('item', row.value._id),
                 thumb: path.attachment(row.value._id,
                                        'thumb.' + row.value.extension)
-			}));
-		}
-		send(Mustache.to_html(templates.recent.tail, data));
-	});
+            }));
+        }
+        data.more = path.list('recent', 'recent-added', {skip: 1,
+                                                         startkey: lastKey,
+                                                         descending: true,
+                                                         limit: 50});
+        send(Mustache.to_html(templates.recent.tail, data));
+    });
 }
